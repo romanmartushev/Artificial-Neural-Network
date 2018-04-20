3ceab59a3195e8bfeb6554301855bdab33c2ebf6
Roman <romanm@paulbunyan.net>
2018-04-20 15:06:39 -0500

    got ANN working for f(x) = 2x

--- ANN.py
+++ ANN.py
@@ -6,23 +6,29 @@ from math import *
 def main():
     count = 0
     j = 0
-    same = 0.0
-    alpha = .05
-    delta = 0.0
+    alpha = .09
     value = 0.0
     outputList = []
+    outputList1 = []
     outputs = []
     RMSE = 0.0
+    oldRMSE = 0.0
     errors = []
-    max = 20.0
-    min = -20.0
+    max = 0.0
+    min = 0.0
     # Array for each hidden node with as many values input + bias nodes
-    weightsHI = [[1,-.5],[.25,-1]]
+    weightsHI = [[uniform(-1,1),uniform(-1,1)],[uniform(-1,1),uniform(-1,1)]]
     # Array for each output node with as many values as hidden nodes + bias Nodes
-    weightsOH = [[1,-1,1]]
+    weightsOH = [[uniform(-1,1),uniform(-1,1),uniform(-1,1)]]
     nodesI = [InputNode(),BiasNode()]
     nodesH = [HiddenNode(),HiddenNode(),BiasNode()]
     nodesO = [OutputNode()]
+
+    myData =[["input","output","actual"]]
+    myFile = myFile = open('outputs.csv','a')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(myData)
 
     #data = open("small.csv",'r')
     data = open("f(x)2x.csv",'r')
@@ -30,64 +36,63 @@ def main():
     with data as d:
         reader = csv.reader(d)
         for row in reader:
-            print(row)
+            outputList1.append(float(row[0]))
+    max = np.amax(outputList1)*2
+    min = np.amin(outputList1)*2
+    #data = open("small.csv",'r')
+    data = open("f(x)2x.csv",'r')
+    #data = open("sin(x).csv",'r')
+    with data as d:
+        reader = csv.reader(d)
+        for row in reader:
             outputList.append(float((float(row[0]) - min)/(max - min)))
-    # with data as d:
-    #     reader = csv.reader(d)
-    #     for row in reader:
-    #         outputList.append(float(row[0]))
 
     counter = len(outputList)
 
-    while count != 10000 and same != 10:
+    while count != 10000:
         ###################################
         #FORWARD
         ###################################
-
-        # reset value to 0
-        value = 0
-
         #Set inputs for all non-bias nodes Input Layer
         for i in range(0,len(nodesI)):
-            if not type(nodesI[i]) is BiasNode:
+            if type(nodesI[i]) is not  BiasNode:
                 nodesI[i].setInput(outputList[j])
 
         #Set inputs for all non-bias nodes Hidden Layer
         for k in range(0,len(nodesH)):
+            value = 0
             if not type(nodesH[k]) is BiasNode:
                 for i in range(0,len(nodesI)):
-                    value =  value + nodesI[i].getInput() * weightsHI[k][i]
-                nodesH[k].setInput(value)
-                value = 0
-
-        #Calculate Output for all non-bias nodes Hidden Layer
-        for i in range(0,len(nodesH)):
-            if not type(nodesH[i]) is BiasNode:
-                nodesH[i].sigmoid()
+                    if type(nodesI[i]) is BiasNode:
+                        value += nodesI[i].getOutput(max,min) * weightsHI[k][i]
+                    else:
+                        value += nodesI[i].getOutput() * weightsHI[k][i]
+                nodesH[k].sigmoid(value)
 
         #Set inputs for Output Layer
         for k in range(0,len(nodesO)):
-            for i in range(0,len(nodesH)):
-                value = value + nodesH[i].getOutput() * weightsOH[k][i]
-            nodesO[k].setInput(value)
             value = 0
-
-        #Calculate Output for Output Layer
-        for i in range(0,len(nodesO)):
-            nodesO[i].sigmoid()
+            for i in range(0,len(nodesH)):
+                if type(nodesH[i]) is BiasNode:
+                    value += nodesH[i].getOutput(max,min) * weightsOH[k][i]
+                else:
+                    value += nodesH[i].getOutput() * weightsOH[k][i]
+            nodesO[k].sigmoid(value)
 
         ###################################
         #CHECK
         ###################################
         for i in range(0,len(nodesO)):
-            outputs.append(nodesO[i].getOutput())
-            errors.append((nodesO[i].getOutput() - outputList[j]*2)**2)
-            if nodesO[i].getOutput() == outputList[j]*2 or abs(nodesO[i].getOutput() - outputList[j]*2) < .01:
-                 same = same + 1
-            else:
-                 same = 0
-            #print("beginning input: " + str(outputList[j] * (max - min) + min) + " current result: " + str(nodesO[i].getOutput() * (max - min) + min) + " actual: " + str((outputList[j] * (max - min) + min)*2) + " iteration: " + str(count))
-            print("beginning input: " + str(outputList[j]) + " current result: " + str(nodesO[i].getOutput()) + " actual: " + str(outputList[j]*2) + " iteration: " + str(count))
+            outputs.append((nodesO[i].getOutput() * (max - min) + min)*2)
+            errors.append(pow((outputList[j] * (max - min) + min)*2 - (nodesO[i].getOutput() * (max - min) + min)*2,2))
+            myData =[[outputList[j] * (max - min) + min,(nodesO[i].getOutput() * (max - min) + min)*2,(outputList[j] * (max - min) + min)*2]]
+            myFile = myFile = open('outputs.csv','a')
+            with myFile:
+                writer = csv.writer(myFile)
+                writer.writerows(myData)
+            print("beginning input: " + str(outputList[j] * (max - min) + min) + " current result: " + str((nodesO[i].getOutput() * (max - min) + min)*2) + " actual: " + str((outputList[j] * (max - min) + min)*2) + " epoch: " + str(count))
+            #print("beginning input: " + str(outputList[j]) + " current result: " + str(nodesO[i].getOutput()) + " actual: " + str(outputList[j]*2) + " iteration: " + str(count))
+
 
         ###################################
         #BACKWARD
@@ -95,24 +100,32 @@ def main():
 
         #Calculate Delta for Output Layer
         for i in range(0,len(nodesO)):
-            nodesO[i].setDelta(outputList[j]*2)
+            nodesO[i].setDelta(outputList[j],max,min)
 
         #Calculate Delta for all non-bias nodes Hidden Layer
         for k in range(0,len(nodesH)):
+            value = 0;
             if not type(nodesH[k]) is BiasNode:
                 for i in range(0,len(nodesO)):
-                    nodesH[k].setDelta(weightsOH[i][k] * nodesO[i].getDelta())
+                    value += nodesO[i].getDelta() * weightsOH[i][k]
+                nodesH[k].setDelta(value)
 
         #Calculate New Weights for Hidden Layer
         for i in range(0,len(nodesO)):
             for k in range(0,len(nodesH)):
-                weightsOH[i][k] = weightsOH[i][k] - (alpha) * nodesO[i].getDelta() * nodesH[k].getOutput()
+                if type(nodesH[k]) is BiasNode:
+                    weightsOH[i][k] = weightsOH[i][k] - (alpha) * nodesO[i].getDelta() * nodesH[k].getOutput(max,min)
+                else:
+                    weightsOH[i][k] = weightsOH[i][k] - (alpha) * nodesO[i].getDelta() * nodesH[k].getOutput()
 
         #Calculate New Weights for Input Layer
         for i in range(0,len(nodesH)):
             if not type(nodesH[i]) is BiasNode:
                 for k in range(0,len(nodesI)):
-                    weightsHI[i][k] = weightsHI[i][k] - 2*(alpha) * nodesH[i].getDelta() * nodesI[k].getOutput()
+                    if type(nodesI[k]) is BiasNode:
+                        weightsHI[i][k] = weightsHI[i][k] - 2*(alpha) * nodesH[i].getDelta() * nodesI[k].getOutput(max,min)
+                    else:
+                        weightsHI[i][k] = weightsHI[i][k] - 2*(alpha) * nodesH[i].getDelta() * nodesI[k].getOutput()
         j = j + 1
         if j == counter:
             RMSE = sqrt(np.sum(errors).mean())
@@ -171,7 +184,7 @@ def main():
         writer.writerows(myData)
 
     for i in range(0,len(outputs)):
-        myData = [[outputs[i],(outputList[i])*2]]
+        myData = [[outputs[i],(outputList[i] * (max - min) + min)*2]]
         myFile = myFile = open('final.csv','a')
         with myFile:
             writer = csv.writer(myFile)
